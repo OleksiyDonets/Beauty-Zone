e89c79cec2db26fbf23267e0d98258e5f30d7f44
OleksiyDonets <161854490+OleksiyDonets@users.noreply.github.com>
2024-05-09 15:44:59 +0300

    Add files via upload

---
+++ JS-Homework-6/js/script-1.js
@@ -0,0 +1,28 @@
+// MODYL 6 (Об'єкти та класи)
+
+// class Abonent {
+//     constructor(name, phoneNumber) {
+//         this.name = name;
+//         this.phoneNumber = phoneNumber;
+//     }
+
+//     setPhoneNumber(phoneNumber) {
+//         this.phoneNumber = phoneNumber;
+//     }
+
+//     getAbonentInfo() {
+//         return `Ім'я: ${this.name}, Телефон: ${this.phoneNumber}`;
+//     }
+// };
+
+// const abonent1 = new Abonent("Іван", "+380991234567");
+// const abonent2 = new Abonent("Марія", "+380997654321");
+// const abonent3 = new Abonent("Петро", "+380981234567");
+
+// console.log(abonent1.getAbonentInfo());
+// console.log(abonent2.getAbonentInfo());
+// console.log(abonent3.getAbonentInfo());
+
+// abonent1.setPhoneNumber("+380998765432");
+
+// console.log(abonent1.getAbonentInfo());
