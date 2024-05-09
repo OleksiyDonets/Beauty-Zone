ee1becc7f07a2caff1525575b939a0a64abced18
OleksiyDonets <161854490+OleksiyDonets@users.noreply.github.com>
2024-05-09 15:45:23 +0300

    Add files via upload

---
+++ JS-Homework-7/js/script-1.js
@@ -0,0 +1,23 @@
+// MODYL 7 (ES-6)
+
+// function Calculator() {
+
+//     this.read = function () {
+//         this.a = +prompt('Введіть перше число:', 0);
+//         this.b = +prompt('Введіть друге число:', 0);
+//     };
+
+//     this.sum = function () {
+//         return this.a + this.b;
+//     };
+
+//     this.mul = function () {
+//         return this.a * this.b;
+//     };
+// }
+
+// let calculator = new Calculator();
+// calculator.read();
+
+// alert("Sum=" + calculator.sum());
+// alert("Mul=" + calculator.mul());
