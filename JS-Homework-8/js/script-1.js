ea5bfc11f4fcaa9b671446629ed9f4ae1055c9ed
OleksiyDonets <161854490+OleksiyDonets@users.noreply.github.com>
2024-05-09 15:45:45 +0300

    Add files via upload

---
+++ JS-Homework-8/js/script-1.js
@@ -0,0 +1,19 @@
+// MODYL 8 (DOM)
+
+// let elem = document.querySelector('[data-widget-name]');
+// alert(elem.getAttribute('data-widget-name'));
+// alert(elem.dataset.widgetName);
+
+// Вибираємо елемент з атрибутом data-widget-name
+// const element = document.querySelector('[data-widget-name]');
+
+// // Перевіряємо, чи елемент був знайдений
+// if (element) {
+//     // Читаємо значення атрибута data-widget-name
+//     const widgetName = element.getAttribute('data-widget-name');
+
+//     // Виводимо значення атрибута на консоль
+//     console.log('Значення атрибута data-widget-name:', widgetName);
+// } else {
+//     console.log('Елемент з атрибутом data-widget-name не знайдено.');
+// }
